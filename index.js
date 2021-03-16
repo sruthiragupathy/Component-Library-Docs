d8a82172c3d61167d578eb0099a7b0cfcb17ccd2
sruthiragupathy <sruthiragupathy@gmail.com>
2021-03-16 16:45:36 +0530

    refactor - Refactor to make the code responsive

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const hamburgerEl = document.querySelector(".hamburger-menu");
+const mobilemenuEl = document.querySelector(".menu__mobile-none");
 
 //functions
-const hamburgerClickHandler = () => (
+const hamburgerClickHandler = () => {
+    console.log("hi")
+    
     hamburgerEl.classList.contains("click") ? 
         hamburgerEl.classList.remove("click") : hamburgerEl.classList.add("click")
-  )
+    mobilemenuEl.classList.contains("menu__mobile") ? 
+        mobilemenuEl.classList.remove("menu__mobile") : mobilemenuEl.classList.add("menu__mobile")
+}
 
 //EventListeners
 hamburgerEl.addEventListener("click",hamburgerClickHandler);
