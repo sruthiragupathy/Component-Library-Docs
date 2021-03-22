bf475dd0360da4fcc1d6ee7ba6a81166c396a787
sruthiragupathy <sruthiragupathy@gmail.com>
2021-03-22 17:03:18 +0530

    refactor - Update index.js to toggle overflow-hidden class

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hamburgerEl = document.querySelector(".hamburger-menu");
 const mobilemenuEl = document.querySelector(".menu__mobile-none");
+const bodyEl = document.querySelector("body");
 
 //functions
 const hamburgerClickHandler = () => {
@@ -7,8 +8,15 @@ const hamburgerClickHandler = () => {
     
     hamburgerEl.classList.contains("click") ? 
         hamburgerEl.classList.remove("click") : hamburgerEl.classList.add("click")
-    mobilemenuEl.classList.contains("menu__mobile") ? 
-        mobilemenuEl.classList.remove("menu__mobile") : mobilemenuEl.classList.add("menu__mobile")
+    if(mobilemenuEl.classList.contains("menu__mobile")) { 
+        mobilemenuEl.classList.remove("menu__mobile")
+        bodyEl.classList.remove("hidden-overflow")
+     } 
+     else{
+        mobilemenuEl.classList.add("menu__mobile");
+        bodyEl.classList.add("hidden-overflow")
+     } 
+
 }
 
 //EventListeners
